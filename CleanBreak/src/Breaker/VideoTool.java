48818ed92d354e2a30bc78657cf0e0bc6d461787
ascott189 <a-scott-1@hotmail.com>
2014-07-17 21:57:47 -0400

    Adding already written code

---
+++ CleanBreak/src/Breaker/VideoTool.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Breaker;
+
+/**
+ *
+ * @author a-scott-1
+ */
+public class VideoTool {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+      BreakView.main();
+   }
+   
+   public void Convert() {
+      if (BreakView.ToConvert == null || BreakView.ConvertDest == null) {
+         //BreakView.
+         
+      }
+   }
+   
+}
